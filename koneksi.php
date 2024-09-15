978dc12386fb287bb25f6120fc7c55b51ef430af
baniadam-dev <042528546@ecampus.ut.ac.id>
2024-09-15 23:58:25 +0700

    Update

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // localhost atau nama server
 $username = "root"; // username database
 $password = ""; // password database
-$dbname = "adamcoms_adamcom_db"; // nama database
+$dbname = "adamcom_db"; // nama database
 
 // Membuat koneksi
 $koneksi = new mysqli($servername, $username, $password, $dbname);
